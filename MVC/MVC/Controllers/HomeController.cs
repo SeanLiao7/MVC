25c334921a1058aa47a0042380d303761a7172c5
Sean Liao <seanliao7@gmail.com>
2017-05-13 17:05:39 +0800

    1. Test AutoMapper. 2. Test CsvHelper.

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index( )
-        {
-            return View( );
-        }
-
         public ActionResult About( )
         {
             ViewBag.Message = "Your application description page.";
@@ -23,6 +21,23 @@ namespace MVC.Controllers
         public ActionResult Contact( )
         {
             ViewBag.Message = "Your contact page.";
+
+            return View( );
+        }
+
+        public ActionResult Index( )
+        {
+            using ( var sw = new StreamWriter( @"D:\Test.csv", true, Encoding.UTF8 ) )
+            using ( var writer = new CsvHelper.CsvWriter( sw ) )
+            {
+                var customers = new NORTHWNDEntities( ).Customers.OrderBy( c => c.CustomerID ).ToList( );
+
+                Mapper.Initialize( x => x.CreateMap<Customers, Customers>( ) );
+
+                var result = Mapper.Map<List<Customers>>( customers );
+
+                writer.WriteRecords( result );
+            }
 
             return View( );
         }
